a2ab4b6fe200505003519f380353316b75145982
dhruv <17bmiit075@gmail.com>
2021-03-20 14:25:30 +0530

    part-8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('title','welcome')
 @section('content')
 @parent
+
 <div class="container">
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
@@ -29,7 +30,22 @@
   </div>
 </div>
 
-
+<div class="container mt-4 mb-4">
+    <div class="row">
+    @foreach ($products as $item)
+    <div class="col-md-4">
+    <div class="card mt-3 mb-3" style="width: 18rem;">
+        <img class="card-img-top" src="{{$item['gallery']}}" style="height:18rem !important;" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{$item['name']}}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">add to cart</a>
+        </div>
+    </div>
+    </div>
+    @endforeach
+    </div>
+</div>
 
 
 
